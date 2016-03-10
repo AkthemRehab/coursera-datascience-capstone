1c063a0bfb59b314a9019b1c7a8ba9bff203f044
yeongwei <ywlai@origineit.com>
2016-03-11 01:02:45 +0800

    Finalized week 1 quiz

--- NOTES.md
+++ NOTES.md
@@ -1,6 +1,15 @@
+### 2016.03.11
+
+1. [faster for() loops in R](http://www.r-bloggers.com/faster-for-loops-in-r/)
+	* It is **MUCH** faster to create the results an empty vector of the correct size.
+	* R seems to be slow in allocating memory for objects.
+2. [Speed up the loop operation in R](http://stackoverflow.com/questions/2908822/speed-up-the-loop-operation-in-r)
+	* The power of vectorization.
+	* Use vectorized commands, e.g. `ifelse` or `diff` is faster than the `apply` family. 
+
 ### 2016.03.10
 
 1. [Text Mining Infrastructure in R](https://www.jstatsoft.org/article/view/v025i05)
-  * Consist of R Source Package and example.
+  	* Consist of R Source Package and example.
 2. [https://cran.r-project.org/web/views/NaturalLanguageProcessing.html](https://cran.r-project.org/web/views/NaturalLanguageProcessing.html)
-  * Worth exploring but not immediately useful. 
+  	* Worth exploring but not immediately useful. 
--- quiz/quiz-1-getting-started.R
+++ quiz/quiz-1-getting-started.R
@@ -15,12 +15,61 @@ fileInfo[, "size"] / 1024^2 "[1] 200.4242"
 ## Q2
 ## 1. http://www.r-bloggers.com/easy-way-of-determining-number-of-linesrecords-in-a-given-large-file-using-r/
 ##
-fileName <- paste(dataDirectory, availableLocale[2], "en_US.twitter.txt", sep="\\")
-allLines <- readLines(fileName)
-length(allLines) # [1] 2360148
-class(allLines) # [1] "character"
+enUsTwitterFileName <- paste(dataDirectory, availableLocale[2], "en_US.twitter.txt", sep="\\")
+enUsTwitter <- readLines(enUsTwitterFileName)
+length(enUsTwitter) # [1] 2360148
+# class(enUsTwitter) # [1] "character"
 
 ##
 ## Q3
 ##
-dt <- data.frame(line = allLines)
+q3 <- function(data) {
+  longestLength <- 0
+  for (l in data) {
+    currLength <- nchar(l)
+    if (currLength > longestLength) {
+      longestLength = currLength
+    }
+  }
+  longestLength
+}
+
+enUsBlogsFileName <- paste(dataDirectory, availableLocale[2], "en_US.blogs.txt", sep="\\")
+enUsBlogs <- readLines(enUsBlogsFileName)
+q3(enUsBlogs) # [1] 40835
+
+enUsNewsFileName <- paste(dataDirectory, availableLocale[2], "en_US.news.txt", sep="\\")
+enUsNews <- readLines(enUsNewsFileName)
+q3(enUsNews) # [1] 5760
+
+##
+## Q4
+##
+q4 <- function(data) {
+  numOfLove <- 0
+  numOfHate <- 0
+  for (l in data) {
+    if (grepl("love", l)) {
+      numOfLove <- numOfLove + 1
+    } else if(grepl("hate", l)) {
+      numOfHate <- numOfHate + 1
+    }
+  }
+  print(paste(numOfLove, "/", numOfHate, sep=" "))
+  numOfLove / numOfHate
+}
+q4(enUsTwitter)
+# [1] "90956 / 20725"
+# [1] 4.388709
+
+##
+## Q5
+## http://stackoverflow.com/questions/14348777/how-to-subset-a-vector-of-sentences-containing-a-given-word
+##
+enUsTwitter[grep("biostats", enUsTwitter)]\
+# [1] "i know how you feel.. i have biostats on tuesday and i have yet to study =/"
+
+##
+## Q6
+##
+grep("A computer once beat me at chess, but it was no match for me at kickboxing", enUsTwitter)
