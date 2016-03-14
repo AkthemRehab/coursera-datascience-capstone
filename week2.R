55fd0a32bf5b4b4b30420ba68b3740c077d517d0
yeongwei <ywlai@origineit.com>
2016-03-15 01:21:51 +0800

    Initial commit for week 2

---
+++ week2.R
@@ -0,0 +1,49 @@
+filePathSep <- "\\"
+fileNameSep <- "."
+swiftKeyDirectory <- ".\\data\\Coursera-SwiftKey"
+finalDirectory <- paste(swiftKeyDirectory, "final", sep = filePathSep)
+outputDirectory <- paste(swiftKeyDirectory, "output", sep = filePathSep) 
+locales <- c("de_DE", "en_US", "fi_FI", "ru_RU")
+contexts <- c("blogs", "news", "twitter")
+fileExt <- "txt"
+
+makeFqnOutputFilePath <- function(locale, context) {
+  localeDirectory <- paste(outputDirectory, locale, sep = filePathSep)
+  dir.create(localeDirectory, showWarnings = FALSE, recursive = TRUE)
+  fileName <- paste(locale, context, fileExt, sep = fileNameSep)
+  fqnOutputFileName <- paste(localeDirectory, fileName, sep = filePathSep)
+  fqnOutputFileName
+}
+
+makeReducedData <- function(fileName) {
+  decide <- rbinom(1, 1, 0.5) # half the file
+  contents <- readLines(fileName)
+  newContents <- c()
+  for (line in contents) {
+    if (decide == TRUE) {
+      newContents <- c(newContents, line)
+    }
+  }
+  newContents
+}
+
+writeDataToFile <- function(fileName, data) {
+  write(data, file = fileName) # over write file
+  print(fileName)
+}
+
+for (locale in locales) {
+  for (context in contexts) {
+    fileName <- paste(locale, context, fileExt, sep = fileNameSep)
+    fullQualifiedFileName <- paste(dataDirectory, locale, fileName, sep = filePathSep)
+    if (file.exists(fullQualifiedFileName) == TRUE) {
+      # print(fullQualifiedFileName) 
+      # print(makeFqnOutputFilePath(locale, context))
+      writeDataToFile(
+        makeFqnOutputFilePath(locale, context), 
+        makeReducedData(fullQualifiedFileName))
+    } else {
+      stop("File not found!") 
+    }
+  }
+}
