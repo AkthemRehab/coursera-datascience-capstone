1f541d973e8e5821397140e564b825685ed7f1c0
yeongwei <ywlai@origineit.com>
2016-03-23 20:47:01 +0800

    Removed stopwords transformation

--- week2.R
+++ week2.R
@@ -100,7 +100,7 @@ transformCorpus <- function(corpus) {
   corpus <- tm_map(corpus, tolower)
   corpus <- tm_map(corpus, removePunctuation)
   corpus <- tm_map(corpus, removeNumbers)
-  corpus <- tm_map(corpus, removeWords, stopwords("english"))
+  # corpus <- tm_map(corpus, removeWords, stopwords("english"))
   corpus <- tm_map(corpus, stemDocument)
   corpus <- tm_map(corpus, stripWhitespace)
   corpus <- tm_map(corpus, PlainTextDocument)
