a1a93e73677792b8f424c7246bcee2cde2555c71
yeongwei <ywlai@origineit.com>
2016-04-07 14:10:56 +0800

    Remove spilled over week3 source code

--- nGramAnalysis.R
+++
@@ -1,9 +0,0 @@
-plotNgram <- function(df, titleLabel, xLabel, yLabel) {
-  plot1 <- ggplot(df, aes(x = reorder(Term, -Count), y = Count))
-  plot1 <- plot1 + geom_bar(stat = "identity")
-  plot1 <- plot1 + ggtitle(titleLabel)
-  plot1 <- plot1 + labs(x = xLabel, y = yLabel)
-  plot1 <- plot1 + theme(axis.text.x = element_text(angle = 45, size = 14, hjust = 1), 
-                         plot.title = element_text(size = 20, face = "bold"))
-  plot1
-}
